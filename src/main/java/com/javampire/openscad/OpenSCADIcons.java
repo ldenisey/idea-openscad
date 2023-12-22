f7e943a52069494b79e984595b58e829d9e81da2
ldenisey <ldenisey@gmail.com>
2023-12-22 12:07:01 +0100

    Refacto OpenSCAD logo files

--- src/main/java/com/javampire/openscad/OpenSCADIcons.java
+++ src/main/java/com/javampire/openscad/OpenSCADIcons.java
@@ -5,7 +5,7 @@ import com.intellij.openapi.util.IconLoader;
 import javax.swing.*;
 
 public class OpenSCADIcons {
-    public static final Icon OPENSCAD_LOGO = IconLoader.findIcon("/com/javampire/openscad/icons/openscad.png", OpenSCADIcons.class);
+    public static final Icon OPENSCAD_LOGO = IconLoader.findIcon("/com/javampire/openscad/icons/openSCAD.svg", OpenSCADIcons.class);
     public static final Icon MODULE = IconLoader.findIcon("/com/javampire/openscad/icons/module.png", OpenSCADIcons.class);
     public static final Icon FUNCTION = IconLoader.findIcon("/com/javampire/openscad/icons/function.png", OpenSCADIcons.class);
     public static final Icon ELEMENT_GROUPER = IconLoader.findIcon("/com/javampire/openscad/icons/element_grouper.png", OpenSCADIcons.class);
